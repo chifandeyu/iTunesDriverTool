245662306add9322c94c67b549ef773c9cd5d511
xuyuchang <xuyuchang@zhuanzhuan.com>
2021-06-16 17:05:55 +0800

    title modify

--- main.cpp
+++ main.cpp
@@ -2,23 +2,16 @@
 #include <QApplication>
 #include "itunesservice.h"
 #include "RepairDriver.h"
-#include "SoftMgr.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    //iTunesServiceCheck sv;
-    //sv.InitEnvironment();
-    //bool appSupport = sv.checkAAS32();
-    //bool appSupport64 = sv.checkAAS64();
-    //bool mobileDeviceSupport = sv.checkAMDS32();
-    //bool mobileDeviceSupport64 = sv.checkAMDS64();
-
     RepairDriver rd;
-    rd.show();
+    //auto check all driver
     bool isOk = rd.checkAllDriver();
     if (!isOk) {
         rd.slotDoRepair();
     }
+    rd.show();
     return a.exec();
 }
